cfa9bd6e9507bbb08bf7bb669f48b4e8e980e318
LuxScape <david.zx360@gmail.com>
2023-10-11 15:12:30 -0500

    Version inicial pantalla login

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,6 +1,56 @@
+import './LoginCss.css'
+import {useState} from "react";
 function PanLogin(){
+
+    const [Action,setAction]=useState("Registrarse");
     return(
-        <h1>Hola</h1>
+        <>
+            <div className='Container'>
+                <div className="header">
+                    <div className="Text">{Action}</div>
+                    <div className="Underline"></div>
+                </div>
+                <div className="Inputs">
+                    {Action==="Iniciar sesion"?<div></div>:<div className="Input">
+                        <img src="" alt=""/>
+                        <input type="text" placeholder="Nombre"/>
+                    </div>
+                    }
+                    {Action==="Iniciar sesion"?<div></div>:<div className="Input">
+                        <img src="" alt=""/>
+                        <input type="text" placeholder="Documento"/>
+                    </div>
+                    }
+                    <div className="Input">
+                        <img src="" alt=""/>
+                        <input type="email" placeholder="Correo electronico"/>
+                    </div>
+                    {Action==="Iniciar sesion"?<div></div>:<div className="Input">
+                        <img src="" alt=""/>
+                        <input type="text" placeholder="Direccion"/>
+                    </div>
+
+                    }
+                    <div className="Input">
+                        <img src="" alt=""/>
+                        <input type="password" placeholder="Contraseña"/>
+                    </div>
+                    {Action==="Iniciar sesion"?<div></div>:<div className="Input">
+                        <img src="" alt=""/>
+                        <input type="password" placeholder="Confirmar contraseña"/>
+                    </div>
+                    }
+                </div>
+                {Action==="Registrarse"?<div></div>:<div className="olvideContraseña"><span>Olvidé mi contraseña</span></div>
+                }
+                <div className="submit-container">
+                    <div className={Action==="Iniciar sesion"?"submit gray":"submit"} onClick={()=>{setAction("Registrarse")}}>Registrarse</div>
+                    <div className={Action==="Registrarse"?"submit gray":"submit"} onClick={()=>{setAction("Iniciar sesion")}}>Iniciar sesion</div>
+                </div>
+            </div>
+        </>
 
     )
 }
+
+export default PanLogin;
