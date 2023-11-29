5727aa8fa4366276b4e8785bd50d919aa1d36cd0
LuxScape <david.zx360@gmail.com>
2023-11-29 15:34:14 -0500

    Update ModelsUser.py

--- backend/Login/ModelsUser.py
+++ backend/Login/ModelsUser.py
@@ -1,59 +1,61 @@
 from utils.Database import Database
 from sqlalchemy import table, column
+
+
 class User:
-    usuarios=table("usuarios",
-                  column("id"),
-                  column("nombre"),
-                   column('apellido'),
-                  column("direccion"),
-                  column("email"),
-                  column("password"),
-                  column("fecha_registro")
-                  )
+    usuarios = table("usuarios",
+                     column("id"),
+                     column("nombre"),
+                     column('apellido'),
+                     column("direccion"),
+                     column("email"),
+                     column("password"),
+                     column("fecha_registro")
+                     )
 
+    @classmethod
     def setUsuario(cls, Nombre, Apellido, Documento, Direccion, Email, Password):
-        db=Database()
+        db = Database()
 
-        NewUsuario=db.insert(cls.usuarios,return_id=False,
-                             id=Documento,
-                             nombre=Nombre,
-                             apellido=Apellido,
-                             direccion=Direccion,
-                             email=Email,
-                             password=Password,
-                             fecha_registro="NOW()"
-                             )
+        NewUsuario = db.insert(cls.usuarios, return_id=False,
+                               id=Documento,
+                               nombre=Nombre,
+                               apellido=Apellido,
+                               direccion=Direccion,
+                               email=Email,
+                               password=Password,
+                               fecha_registro="NOW()"
+                               )
 
         db.commit()
         db.close()
 
+    def getUsuario(self, Nombre):
+        db = Database()
 
-    def getUsuario(self,Nombre):
-        db=Database()
-
-        SQL="""
+        SQL = """
         SELECT *
         FROM usuarios 
         WHERE nombre=Nombre;
             """
 
-        datoU=db.query(SQL,Nombre)
+        datoU = db.query(SQL, Nombre)
 
         db.close()
 
         return datoU
 
-    def loginChecker(self,Email,Password):
-        db=Database()
-        SQL="""
+    def loginChecker(self, Email, Password):
+        db = Database()
+        SQL = """
         SELECT *
         FROM usuarios
         WHERE email=Email
         AND password=Password;
         """
-        DatoP=db.query(SQL,Email,Password)
+        DatoP = db.query(SQL, Email, Password)
 
-        if DatoP.len!=0:
+        if DatoP.len != 0:
             return True
         else:
             return False
