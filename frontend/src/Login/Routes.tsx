9722606b850b9feae233d89f318e3fa74a0f967d
LuxScape <david.zx360@gmail.com>
2023-11-30 14:20:44 -0500

    Path por defecto ahora redirige a login

--- frontend/src/Login/Routes.tsx
+++ frontend/src/Login/Routes.tsx
@@ -1,12 +1,13 @@
-import { RouteObject } from "react-router-dom";
-
+import {RouteObject, Navigate} from "react-router-dom";
 import RecuperarCuenta from "./RecuperacionCuenta";
 import NuevaContraseña from "./NuevaContraseña";
 import FinRegistro from "./FinRegistro";
 import PanLogin from "./Login";
 import PanRegister from "./Register";
 
-
+const NavPanLogin=()=>{
+    return <Navigate to={{pathname:'/Login'}}/>
+}
 export const routes: RouteObject[] = [
     {
         path: '/Login',
@@ -28,5 +29,10 @@ export const routes: RouteObject[] = [
     {
         path:'/Register',
         element: <PanRegister/>
+    },
+    {
+        path:'*',
+        element: <NavPanLogin/>
+
     }
 ]
