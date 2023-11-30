e3ad5d757d5412071cc495c7228583409a8030cf
LuxScape <david.zx360@gmail.com>
2023-11-30 14:20:07 -0500

    Login app added

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,12 @@ from flask_cors import CORS
 
 from Ventas.routes import Ventas
 from Admin.routes import Admin
+from Login.RoutesUser import Login
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(Ventas)
 app.register_blueprint(Admin)
+app.register_blueprint(Login)
 # registrar demas modulos
